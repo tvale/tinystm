50eade9ecdb5d668a8afd87e93e805e71d5e8951
joaop <95joaopereira@gmail.com>
2016-04-28 11:37:40 +0100

    teste

--- src/stm_wbctl.h
+++ src/stm_wbctl.h
@@ -182,7 +182,8 @@ stm_wbctl_read(stm_tx_t *tx, volatile stm_word_t *addr)
     /* In irrevocable mode, no need check timestamp nor add entry to read set */
     if (tx->irrevocable){
 
-      SET_READ_TS(addr, _tinystm.privileged_ts)
+      // set-read-ts(x, privileged-ts)
+
 
       // wait until is-unlocked(x)
 
@@ -190,7 +191,7 @@ stm_wbctl_read(stm_tx_t *tx, volatile stm_word_t *addr)
     }
 #endif /* IRREVOCABLE_ENABLED */
     /* Check timestamp */
-    version = LOCK_GET_TIMESTAMP(l);
+    version = LOCK_GET_TIMESTAMP(l); //need read and write timestamps for each object lock (write-ts and read-ts)
     /* Valid version? */
     if (version > tx->end) { // if write-ts(x) > valid-ts
       //or (write-ts(x) > privileged-ts and parity(write-ts(x)) = parity(privileged-ts))
